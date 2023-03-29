172d05de0599964767ac910ca814d2151f9a1742
JosielMatos <josielmatos77@protonmail.com>
2023-03-29 09:09:49 -0300

    Added register page passwords matching validation

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,13 +1,33 @@
+import { FormEvent, useState } from "react";
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
 
 import styles from "./styles.module.css";
 
 export function Register() {
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [passwordsMatch, setPasswordsMatch] = useState(true);
+
+  function handlePasswordChange(e: FormEvent<HTMLInputElement>) {
+    setPassword(e.currentTarget.value);
+  }
+
+  function handleConfirmPasswordChange(e: FormEvent<HTMLInputElement>) {
+    setConfirmPassword(e.currentTarget.value);
+  }
+
   const text = "Por favor, registre-se para continuar";
 
-  function validate() {
-    //todo
+  function validate(event: FormEvent) {
+    event.preventDefault();
+
+    if (password !== confirmPassword) {
+      setPasswordsMatch(false);
+      return;
+    }
+
+    alert("All good!");
   }
 
   return (
@@ -20,33 +40,52 @@ export function Register() {
             <input
               type='text'
               placeholder='Nome'
+              required
               className={`${styles.input} ${styles["name-field"]}`}
             />
             <input
               type='text'
               placeholder='Usuário'
+              required
               className={`${styles.input} ${styles["user-field"]}`}
             />
             <input
               type='text'
               placeholder='Nascimento'
+              required
               className={`${styles.input} ${styles["birth-field"]}`}
             />
             <input
-              type='text'
+              type='email'
               placeholder='Email'
+              required
               className={`${styles.input} ${styles["email-field"]}`}
             />
             <input
               type='password'
               placeholder='Senha'
+              pattern='.{8,}'
+              title='Mínimo de 8 caracteres'
+              required
+              onChange={handlePasswordChange}
               className={`${styles.input} ${styles["password-field"]}`}
             />
             <input
               type='password'
               placeholder='Confirmar Senha'
+              pattern='.{8,}'
+              title='Mínimo de 8 caracteres'
+              required
+              onChange={handleConfirmPasswordChange}
               className={`${styles.input} ${styles["confirm-password-field"]}`}
             />
+
+            {!passwordsMatch && (
+              <p className={styles["passwords-dont-match"]}>
+                As senhas não correspondem!
+              </p>
+            )}
+
             <Button label='Registrar-se' />
           </form>
           <p className={styles["form-bottom"]}>
