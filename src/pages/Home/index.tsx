e55af9b1d8f35ea361811dc392245c1e2aeb50f4
JosielMatos <josielmatos77@protonmail.com>
2023-04-25 15:59:34 -0300

    feat: add home page nav and header

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,33 @@
+import styles from "./styles.module.css";
+import homeIcon from "../../assets/home-icon.svg";
+
 export function Home() {
-  return <h1>This is the Home Page</h1>;
+  return (
+    <div className={styles.wrapper}>
+      <nav className={styles.nav}>
+        <img
+          src='/compass-uol-negativo.png'
+          alt='logo compass'
+          className={styles.logo}
+        />
+      </nav>
+      <main className={styles.main}>
+        <header className={styles.header}>
+          <div className={styles["header-item"]}>
+            <img src={homeIcon} alt='Home' />
+            <h2>Home</h2>
+          </div>
+
+          <div className={styles["header-item"]}>
+            <p>Eduardo Pereira</p>
+            <img
+              className={styles["profile-picture"]}
+              src='https://wallpapercave.com/wp/wp7151807.jpg'
+              alt='Profile picture'
+            />
+          </div>
+        </header>
+      </main>
+    </div>
+  );
 }
